70e0697d2e4cf3f04c0e6b1fe521610cccd6e3a7
Anderson Begossi <anderson.begossi@spartalabs.com.br>
2023-06-24 11:29:45 -0300

    chore: init project

---
+++ App.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+  NativeModules,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const {CalendarModule} = NativeModules;
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const createCalendarEventAndroid = () => {
+    CalendarModule.createCalendarEvent('Aniversário', 'Minha casa');
+  };
+
+  return (
+    <SafeAreaView style={styles.safeAreaView}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={isDarkMode ? Colors.darker : Colors.lighter}
+      />
+      <ScrollView contentContainerStyle={styles.scrollViewContentContainer}>
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={createCalendarEventAndroid}>
+            <Text>Criar Evento no Calendário (Android)</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  container: {
+    backgroundColor: Colors.lighter,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollViewContentContainer: {
+    flex: 1,
+  },
+  button: {
+    padding: 16,
+    backgroundColor: 'white',
+    borderRadius: 12,
+  },
+});
+
+export default App;
